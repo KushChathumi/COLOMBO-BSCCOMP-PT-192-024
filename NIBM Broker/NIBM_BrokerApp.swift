b526a38f7039f02a66eab2cc13e1b56153b47a2d
Kushani Abeysinghe <kushani96abeysinghe@gmail.com>
2022-04-01 15:34:55 +0530

    Initial Commit

---
+++ NIBM Broker/NIBM_BrokerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NIBM_BrokerApp.swift
+//  NIBM Broker
+//
+//  Created by Kushani Abeysinghe on 2022-04-01.
+//
+
+import SwiftUI
+
+@main
+struct NIBM_BrokerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
